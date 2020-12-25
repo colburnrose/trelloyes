3a61a1ba541de36d47b000d03d0a033a0a6ff3cd
colburncodes <colburnsanders@gmail.com>
2020-12-25 14:40:21 -0800

    implemented delete functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import List from "./List/List";
 import "./App.css";
+import STORE from "./store";
+
+function omit(obj, keyToOmit) {
+  let { [keyToOmit]: _, ...rest } = obj;
+  return rest;
+}
 
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    },
+  state = {
+    store: STORE,
   };
+
+  handleDeleteItem = (cardId) => {
+    console.log("handle delete item clicked", { cardId });
+    const { lists, allCards } = this.state.store;
+    const cardlist = lists.map((list) => ({
+      ...list,
+      cardIds: list.cardIds.filter((id) => id !== cardId),
+    }));
+
+    const newCards = omit(allCards, cardId);
+
+    this.setState({
+      store: {
+        lists: cardlist,
+        allCards: newCards,
+      },
+    });
+  };
+
   render() {
-    const { store } = this.props;
+    const { store } = this.state;
     return (
       <main className="App">
         <header>Trelloyes!</header>
@@ -18,8 +40,10 @@ class App extends Component {
           {store.lists.map((list) => (
             <List
               key={list.id}
+              id={list.id}
               header={list.header}
               cards={list.cardIds.map((id) => store.allCards[id])}
+              onDeleteItem={this.handleDeleteItem}
             />
           ))}
         </div>
